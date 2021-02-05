efdd8a9ac0a1a0eacce85441a49e3dd855db2684
Lucas <lucasgrz73@gmail.com>
2021-02-05 04:33:45 +0000

    Made it display in the screen instead of the console.

--- mario.js
+++ mario.js
@@ -1,35 +1,11 @@
-// TODO #2
-// Take in user input for the height
 height = parseInt (prompt("What is the height?"));
-/*
- * printPyramid
- *
- * Prints to the console a pyramid of '#' characters of the specified height
- * For example, if height is 5, the console will look like this:
- *          ##
- *         ###
- *        ####
- *       #####
- *      ######
- */
-    // TODO #1
-    // print that pyramid!
-console.log("--------------------");
-console.log("Example - Height: 5")
-console.log("    ##");
-console.log("   ###");
-console.log("  ####");
-console.log(" #####");
-console.log("######");
-console.log("--------------------");
-
-console.log("Height entered: " + height)
 if (height > 0) {
 let pyramid = []
 function printPyramid(height) {
-let space = " ";
+let space = " . ";
 let brick = "#";
 let layer = "";
+let BR = "<br />";
     a = height;
 	  for(b = height; b > 0; b--){
 		heightHeight = height + 1;
@@ -40,7 +16,7 @@ let layer = "";
 			layer = space + layer;
 			}
 		let e = b - 1
-		pyramid[e]=(layer);
+		pyramid[e]=(layer + BR);
 		layer = "";
 		height = height - 1;
 	    }
@@ -48,10 +24,6 @@ let layer = "";
     printPyramid(height);
     f = height;
     for(g = 0; g < f; g++){
-	  console.log(pyramid[g]);
+	  document.write(pyramid[g]);
       }
 }
-if (height <= 0) {
-  console.log("Number must be greater than 0.");
-}
-console.log("--------------------");
