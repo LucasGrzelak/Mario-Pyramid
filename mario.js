38da238eb8a9e4501d93d5516843eb0c36972562
Lucas <lucasgrz73@gmail.com>
2021-03-18 03:33:34 +0000

    Update to Project 3

--- mario.js
+++ mario.js
@@ -1,29 +1,34 @@
-height = parseInt (prompt("What is the height?"));
-if (height > 0) {
-let pyramid = []
-function printPyramid(height) {
-let space = " . ";
-let brick = "#";
-let layer = "";
-let BR = "<br />";
-    a = height;
-	  for(b = height; b > 0; b--) {
-		heightHeight = height + 1;
-		for(c = 0; c < heightHeight; c++) {
-			layer = layer + brick;
-			}
-		for(d = a; d > height; d--) {
-			layer = space + layer;
-			}
-		let e = b - 1
-		pyramid[e]=(layer + BR);
-		layer = "";
-		height = height - 1;
-	    }
-}
+document.getElementById("button").onclick = function printPyramid(height) {
+  var height = document.getElementById("height").value;
+  if (height > 0) {
+    document.getElementById("pyramid").innerHTML = "";
+    var pyramid = []
+    function printPyramid(height) {
+      height = parseInt(height) + 1;
+      let space = ".";
+      let brick = "#";
+      let layer = "";
+      let BR = "<br />";
+        a = height;
+        for(b = height; b > 0; b--) {
+          heightHeight = height + 1;
+          for(c = 0; c < heightHeight; c++) {
+            layer = layer + brick;
+            }
+          for(d = a; d > height + 1; d--) {
+            layer = space + layer;
+            }
+          let e = b - 1
+          pyramid[e]=(layer + BR);
+          layer = "";
+          height = height - 1;
+            }
+    }
     printPyramid(height);
     f = height;
-    for(g = 0; g < f; g++){
-	  document.write(pyramid[g]);
+    for(g = 0; g < f; g++) {
+      document.getElementById("pyramid").innerHTML += "<p>" + pyramid[g] + "</p>";
+      console.log(pyramid[g])
       }
+  }
 }
